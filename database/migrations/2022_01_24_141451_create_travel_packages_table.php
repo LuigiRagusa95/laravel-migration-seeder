7fca3e1f996eca263bb06e2aa403d0dce7003a3b
LuigiRagusa95 <luigiragusa99@gmail.com>
2022-01-24 15:45:02 +0100

    ADD: add travel_packages migration

---
+++ database/migrations/2022_01_24_141451_create_travel_packages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelPackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travel_packages', function (Blueprint $table) {
+            $table->id();
+            $table->smallInteger('room');
+            $table->smallInteger('travellers');
+            $table->string('going_to', 200);
+            $table->string('leaving_from', 200);
+            $table->timestamp('departing')->nullable()->default(null);
+            $table->timestamp('returning')->nullable()->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travel_packages');
+    }
+}
