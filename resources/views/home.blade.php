69983910dd1ae05ffa16273ef69154aa4babaf91
LuigiRagusa95 <luigiragusa99@gmail.com>
2022-01-24 16:30:31 +0100

    ADD: some html output

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,19 @@
 </head>
 
 <body>
-    @dump($packages)
+    @foreach($packages as $package)
+    <article>
+        <p><strong>Going to:</strong> {{ $package->going_to }}</p>
+        <p><strong>Leaving from:</strong> {{ $package->leaving_from }}</p>
+        <p><strong>Departing:</strong> {{ $package->departing }}</p>
+        <p><strong>Returning:</strong> {{ $package->returning }}</p>
+        <p><strong>Room:</strong> {{ $package->room }}</p>
+        <p><strong>Travellers:</strong> {{ $package->travellers }}</p>
+        @if(!$loop->last)
+        <hr />
+        @endif
+    </article>
+    @endforeach
 </body>
 
 </html>
